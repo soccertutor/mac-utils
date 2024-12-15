32932a1b4e3afdbc23670d560de943e84e553e34
Alexander Gordeyko <axgord@gmail.com>
2024-12-15 22:24:12 +0200

    Added transferDataFromDocumentsToAppContainer method

--- extension/macutils/MacUtils.hx
+++ extension/macutils/MacUtils.hx
@@ -5,25 +5,26 @@ import openfl.events.Event;
 import openfl.events.EventDispatcher;
 
 class MacUtils {
-	
+
 	private static var initialized:Bool = false;
 	private static var dispatcher = new EventDispatcher ();
 
-	public static function addEventListener (type:String, listener:Dynamic, useCapture:Bool = false, priority:Int = 0, useWeakReference:Bool = false):Void {
+	public static inline function addEventListener (type:String, listener:Dynamic, useCapture:Bool = false, priority:Int = 0, useWeakReference:Bool = false):Void {
 		dispatcher.addEventListener (type, listener, useCapture, priority, useWeakReference);
 	}
-	
-	public static function removeEventListener (type:String, listener:Dynamic):Void {
+
+	public static inline function removeEventListener (type:String, listener:Dynamic):Void {
 		dispatcher.removeEventListener (type, listener);
 	}
-	
-	public static function dispatchEvent (event:Event):Bool {
+
+	public static inline function dispatchEvent (event:Event):Bool {
 		return dispatcher.dispatchEvent (event);
 	}
-	
-	public static function hasEventListener (type:String):Bool {
+
+	public static inline function hasEventListener (type:String):Bool {
 		return dispatcher.hasEventListener (type);
 	}
+
 	private static function notifyListeners (inEvent:Dynamic) {
 		#if mac
 		var type = Std.string (Reflect.field (inEvent, "type"));
@@ -40,17 +41,24 @@ class MacUtils {
 		}
 		#end
 	}
-	
-	public static function saveFileWithType (data:String, type:String) {
+
+	public static inline function saveFileWithType (data:String, type:String) {
 		#if mac
 		return mac_utils_save_file_with_type(data, type);
 		#end
 	}
-	
+
+	public static inline function transferDataFromDocumentsToAppContainer (prompt:String, message:String, dataFolderName:String, checkFolders:String, checkFiles:String):Bool {
+		#if mac
+		return mac_utils_transfer_data_from_documents_to_app_container(prompt, message, dataFolderName, checkFolders, checkFiles);
+		#end
+	}
+
 	#if mac
 	private static var mac_utils_save_file_with_type = Lib.load ("mac_utils", "mac_utils_save_file_with_type", 2);
+	private static var mac_utils_transfer_data_from_documents_to_app_container = Lib.load ("mac_utils", "mac_utils_transfer_data_from_documents_to_app_container", 5);
 	private static var mac_utils_set_event_handle = cpp.Lib.load ("mac_utils", "mac_utils_set_event_handle", 1);
 	#end
-	
-	
+
+
 }
--- project/include/MacUtils.h
+++ project/include/MacUtils.h
@@ -3,11 +3,12 @@
 
 
 namespace mac_utils {
-	
+
 	extern "C" {
 		void saveFileWithType(const char *data, const char *type);
+		bool transferDataFromDocumentsToAppContainer(const char *prompt, const char *message, const char *dataFolderName, const char *checkFolders, const char *checkFiles);
 	}
-	
+
 }
 
 
